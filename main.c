2bcffa62b3ca25eada261aa29e643703f72261b8
pach <949342769@qq.com>
2020-04-01 12:37:04 +0800

    添加了删除指定节点函数

--- main.c
+++ main.c
@@ -48,11 +48,13 @@ LinkedList* SearchList(int data)//搜索函数(指定数据)
     {
         if (data == p->data)
         {
-            return (printf("查找成功,该节点地址为 %p",p));//返回指针地址返回指针地址
+            printf("查找成功,该节点地址为 %p",p);
+            return p;//返回指针地址返回指针地址
         }
         p=p->next;
     }
-    return (printf("没有这样的节点"));
+    printf("没有这样的节点");
+    return NULL;
 }
 
 void DestroyList(LinkedList *head)//清空链表
@@ -68,20 +70,69 @@ void DestroyList(LinkedList *head)//清空链表
     end  = NULL;
 }
 
-void DeleteList(int data)//删除指定节点
+void DeleteList(int data_p, int data_e)//删除指定节点
 {
-    LinkedList *p=head;
-    while (p != NULL)
+    if (head == NULL)//如果链表为空
     {
-        if(p->next = data)
-        {
-
-        }
-        free(p);
+        printf("链表为空，请先初始化\n");
+        return;
     }
-    
+    LinkedList *p = SearchList(data_p);
+    if (p == NULL)//如果没有该节点信息
+    {
+        printf("没有该节点信息存在\n");
+        return;
+    }
+    else if (head == end)//如果存在但只有一个节点
+    {
+        free(head);
+        head = NULL;
+        end  = NULL;
+    }
+    else if (head->next == end)//只有两个节点，判断头尾
+    {
+        if (end == p)
+        {
+            end = head;
+            end->next = NULL;//搜索知道是的end，end指向head
+            ;//释放end节点
+            free(p);
+        }
+        else if (head == p)//同理
+        {
+            head == end;
+            head->next == NULL;
+            free(p);
+        }
+    }
+    else//多个节点情况
+    {
+        if (p == head)//删除头
+        {       
+            head = head->next;
+            free(p);
+        }
+        else if (p == end)//删除尾
+        {
+            LinkedList *LastButone = head;
+            while (LastButone->next != end)//找到倒数第二个节点
+                {LastButone = LastButone->next;}
+            free(end);
+            end = LastButone;
+            end->next = NULL;
+        }
+        else//删除中间节点
+        {
+            LinkedList *Former = head;
+            while (Former->next != p)//找到前一个节点
+                {Former = Former->next;}
+            Former->next = p->next;
+            free(p);
+        }   
+    }    
 }
-void print()
+
+void print()//遍历输出所有节点
 {
     LinkedList *p=head;
     int count = 0;
@@ -93,10 +144,34 @@ void print()
     }
 }
 
-void InsertList()
+void InsertList(int q,int data)//从后插入节点
 {
-    
+    if (head = NULL)
+    {
+        printf("链表没有节点");
+        return;
+    }
+    LinkedList *p = SearchList(data);
+    if (p == NULL)
+    {
+        printf("没有该节点");
+        return;
+    }
+    LinkedList *temp = (LinkedList*)malloc(sizeof(LinkedList));
+    temp->next = q;
+    temp->next = NULL;
+    if (p == end)//如果找到的节点在尾部
+    {   
+        end->next = temp;
+              end = temp;
+    }
+    else//如果在中间（包含头部情况）
+    {
+       temp->next = p->next; //先连后面
+          p->next = temp;  //再连前面
+    }
 }
+
 int main()
 {
     int data;
